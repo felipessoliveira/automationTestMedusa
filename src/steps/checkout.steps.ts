9e7f113649772c3c500f6935b53925a1abec9114
Felipe Oliveira <felipessoliveira@gmail.com>
2026-06-02 22:27:59 +0100

    EP-9: repair QA TC automation after validation attempt 2 for src/steps/checkout.steps.ts

--- src/steps/checkout.steps.ts
+++ src/steps/checkout.steps.ts
@@ -16,6 +16,7 @@ Given('a customer has items in their cart', async function (this: CustomWorld) {
   const pdp = new ProductPage(this.page);
   if (await pdp.canAddToCart()) {
     await pdp.addToCart();
+    await this.page.waitForTimeout(2000);
   } else {
     throw new Error('Product cannot be added to cart');
   }
